86c8051e7d8117fa9046fbaed482532e81bd5fe8
NilayBadjatya <147936252+NilayBadjatya@users.noreply.github.com>
2024-08-25 20:14:41 +0530

    Add files via upload

---
+++ src/components/Addnotes.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const Addnotes = (props) => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({ title: "", description: "", tag: "" });
+  const handleClick = (e) => {
+    e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+    setNote({ title: "", description: "", tag: "" });
+    props.showAlert("Note Created Successfully ", "success");
+  };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="container">
+      <h1 align="center" style={{ margin: "15px 0px" }} className="myH1">
+        Add a Note
+      </h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={note.title}
+            name="title"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            name="description"
+            value={note.description}
+            onChange={onChange}
+            minLength={5}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="tag" className="form-label">
+            Tag
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            value={note.tag}
+            id="tag"
+            name="tag"
+            onChange={onChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Addnotes;
